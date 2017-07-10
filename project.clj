58dfe7ef48fe921a0f499c96101e62c3bb83df66
didi <_git@d10r.net>
2017-07-10 11:49:46 +0200

    fixed project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ws "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject ws "0.2.0"
+  :description "A minimal Blockchain in Clojure"
+  :url "https://github.com/d10r/l10chain"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
